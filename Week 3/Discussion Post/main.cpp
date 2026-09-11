2ca5febae70f56a8d05723141177262dab2282bd
Harry L. <harry@spolem.net>
2026-09-11 17:02:44 -0500

    Upload Week 3 discussion post.
    This week's discussion post contains two purposeful errors. Let me know if you find them and how you would solve them!

---
+++ Week 3/Discussion Post/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+// Harry Laack
+// Week 3 Discussion Post
+// 09/11/2026
+
+int main() {
+
+    std::cout << "Welcome to the Week 3 Discussion Post!" << std::endl;
+    std::cout << "There are two errors about. Can you find them?" << std::endl;
+
+    bool isValid = false;
+
+    if (!isValid) {
+        std::cout << "Please enter a valid input." << std::endl;
+    }
+    else if {
+        std::cout << "Please enter a valid input." << std::endl;
+    }
+    else {
+        std::cout << "You've entered a valid input." << std::endl;
+    }
+
+    int num_of_hours = 13;
+
+    switch (num_of_hours) {
+        case 12:
+            std::cout << "It is noon." << std::endl;
+            int num_of_minutes = 720;
+            break;
+        case 13:
+            std::cout << "It is 1:00pm" << std::endl;
+            num_of_minutes = 780;
+            break;
+    }
+
+    return 0;
+
+}
